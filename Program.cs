fd5b876929d6d4ac0d377ceb83cda33aa6d24435
Temi Lajumoke <temi@temilajumoke.com>
2018-02-20 23:55:44 +0100

    move role creation to app startup

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NET_core_role_based_authentication.Data;
 
 namespace ASP.NET_core_role_based_authentication
 {
@@ -14,7 +16,26 @@ namespace ASP.NET_core_role_based_authentication
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var serviceProvider = services.GetRequiredService<IServiceProvider>();
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    Seed.CreateRoles(serviceProvider, configuration).Wait();
+                    
+                }
+                catch (Exception exception)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "An error occurred while creating roles");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace ASP.NET_core_role_based_authentication
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -67,47 +67,6 @@ namespace ASP.NET_core_role_based_authentication
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-             CreateRoles(serviceProvider).Wait();
-        }
-
-        private async Task CreateRoles(IServiceProvider serviceProvider)
-        {
-            //adding customs roles
-            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-            string[] roleNames = { "Admin", "Manager", "Member" };
-            IdentityResult roleResult;
-
-            foreach (var roleName in roleNames)
-            {
-                //creating the roles and seeding them to the database
-                var roleExist = await RoleManager.RoleExistsAsync(roleName);
-                if (!roleExist)
-                {
-                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
-                }
-            }
-
-            //creating a super user who could maintain the web app
-            var poweruser = new ApplicationUser
-            {
-                UserName = Configuration.GetSection("AppSettings")["UserEmail"],
-                Email = Configuration.GetSection("AppSettings")["UserEmail"]
-            };
-
-            string userPassword = Configuration.GetSection("AppSettings")["UserPassword"];
-            var user = await UserManager.FindByEmailAsync(Configuration.GetSection("AppSettings")["UserEmail"]);
-
-            if(user == null)
-            {
-                    var createPowerUser = await UserManager.CreateAsync(poweruser, userPassword);
-                    if (createPowerUser.Succeeded)
-                    {
-                        //here we tie the new user to the "Admin" role 
-                        await UserManager.AddToRoleAsync(poweruser, "Admin");
-
-                    }
-            }
         }
     }
 }
